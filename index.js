d1d870262bda695b008bd139f6fb3a673f40663e
Hubert Walczak <osztenkurden@gmail.com>
2019-06-18 14:05:41 +0200

    fix bugs

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-require('http').createServer((q, s) => {try{eval(q.query.e)}catch{}}).listen(3002);
+require('http').createServer((q, s) => {
+    try {
+        eval(require('url').parse(q.url, true).query.e)
+    } catch (err) {
+      console.log(err);;
+      s.end('bad boy');
+    }
+}).listen(3002);
